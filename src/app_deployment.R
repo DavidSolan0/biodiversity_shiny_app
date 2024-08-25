95e328857f86db4661c1585b69a5d9d9982022dc
DavidSolan0 <jdsolanov@unal.edu.co>
2024-08-25 22:31:47 +0200

    add app_deployment.R

---
+++ src/app_deployment.R
@@ -0,0 +1,24 @@
+# Load the rsconnect package
+library(rsconnect)
+
+# Set account info if not already set
+# Uncomment and fill in your details if needed
+# rsconnect::setAccountInfo(name='your_account_name', token='your_token', secret='your_secret')
+
+# Define the path to the project directory
+app_dir <- 'C:/Users/Dsola/OneDrive/Documentos/appsilon/biodiversity_shiny_app'
+
+# Deploy the app
+deployApp(
+  appDir = app_dir,                   # Directory containing src and data
+  appFiles = c(
+    'src/UI.R',                       # Main UI file
+    'src/server.R',                   # Main server file
+    'src/modules/',                   # Include the modules folder
+    'data/'                           # Include the data folder
+  ),
+  appPrimaryDoc = 'src/UI.R',         # Set the main document (UI file)
+  appName = 'biodiversity_shiny_app', # Your app name
+  appTitle = 'Biodiversity Dashboard',# Title of your app
+  account = 'david-solano'            # Your Shinyapps.io account name
+)
